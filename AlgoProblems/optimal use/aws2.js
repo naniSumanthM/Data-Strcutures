b26a0903c62a248a761f7523e375786ec6c5eac2
Sumanth <sumanth9571@gmail.com>
2019-05-13 13:51:30 -0400

    filter

--- AlgoProblems/optimal use/aws2.js
+++ AlgoProblems/optimal use/aws2.js
@@ -32,12 +32,12 @@ let optimalUtilization = (capacity, fApps, bApps) => {
   return output;
 };
 
-// console.log(optimalUtilization(7, [[1, 2], [2, 4], [3, 6]], [[1, 2]]));
+console.log(optimalUtilization(7, [[1, 2], [2, 4], [3, 6]], [[1, 2]]));
 
-// console.log(
-//   optimalUtilization(
-//     10,
-//     [[1, 3], [2, 5], [3, 7], [4, 10]],
-//     [[1, 2], [2, 3], [3, 4], [4, 5]]
-//   )
-// );
+console.log(
+  optimalUtilization(
+    10,
+    [[1, 3], [2, 5], [3, 7], [4, 10]],
+    [[1, 2], [2, 3], [3, 4], [4, 5]]
+  )
+);
