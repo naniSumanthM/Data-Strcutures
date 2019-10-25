8587ce808228e1b10fef8d5ae9baaac8269b59b2
Sumanth <sumanth9571@gmail.com>
2019-10-25 12:35:57 -0400

    latest

--- Code.js
+++ Code.js
@@ -3,7 +3,21 @@ let str = "$um@nth,"
 let str2 = "SUmanth$,^&*()@#$%!~"
 console.log(str2.replace(/[^\w]/g, "").toUpperCase().split("").join(""));
 
-//check map values
-//Array.from(Map.values()).includes(x)
+console.log('SUmanth$,^&*()@#$%!~'.toLocaleLowerCase().match(/\b[\w']+\b/g));
 
-//console.log('SUmanth$,^&*()@#$%!~'.toLocaleLowerCase().match(/\b[\w']+\b/g));
+
+// primaryArray = {1,2,3,4}
+// subArray = {1, 2}
+let getSubArray = (primaryArray, subArray) =>{
+    
+    let indexOutput = [] 
+    
+    for(let i = 0; i < primaryArray.length; i++){
+        for(let j = 0; j < subArray.length; j++){// i = 0, j = 1
+            if(primaryArray[i + j] === subArray[j]){ // primaryArray[1] === 2, subArray[1] === 2
+                indexOutput.push(i)
+            }
+        }
+    }
+    return indexOutput;
+}
